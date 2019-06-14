f709da20b8e2cb5c61c0607ed62baff9b90975bf
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-06-15 01:55:02 +0530

    implemented search video functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+  inputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onFormSubmit(this.state.term)
+  };
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Search video</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.inputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
