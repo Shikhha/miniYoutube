f5b4b4c005b23f0a5a2b311d675d025e0deca572
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-06-14 23:04:22 +0530

    Added initial component structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class SearchBar extends React.Component {
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
